c19de393437e2c9c507a1d3e352b07517750bcfa
egyketted <szabolcs.foti@gmail.com>
2024-01-24 12:40:10 +0100

    Create skeleton for kotlin springboot app

---
+++ src/main/kotlin/com/example/kotlinspringbootgraphql/KotlinSpringBootGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringbootgraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootGraphqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootGraphqlApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringbootgraphql/KotlinSpringBootGraphqlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringbootgraphql
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootGraphqlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
